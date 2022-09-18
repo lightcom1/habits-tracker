eb9cd7373c5e4d4a0bbe003d718ff14ca361d641
Denys Kurylenko <lightcom2001@gmail.com>
2022-09-18 13:31:00 +0300

    added pwa integration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+	plugins: [
+		react(),
+		VitePWA({
+			registerType: 'autoUpdate',
+			includeAssets: ['favicon.png'],
+      manifest: {
+        name: 'HabitsTracker',
+        short_name: 'HabitsTracker',
+        description: 'Habits tracker app',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'brain192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'brain512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+		}),
+	],
+});
